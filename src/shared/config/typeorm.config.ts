54212f1b46ceacc36c344d06db8b03f3ef862b0d
Gustavo Mata <dev.gustta.h@gmail.com>
2023-07-25 18:43:50 -0300

    chore: update package.json version

---
+++ src/shared/config/typeorm.config.ts
@@ -0,0 +1,10 @@
+module.exports = {
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST || 'localhost',
+  port: parseInt(process.env.POSTGRES_PORT, 10) || 5432,
+  username: process.env.POSTGRES_USER || 'myuser',
+  password: process.env.POSTGRES_PASSWORD || 'mypassword',
+  database: process.env.POSTGRES_DB || 'mydb',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: process.env.NODE_ENV === 'development'
+}
