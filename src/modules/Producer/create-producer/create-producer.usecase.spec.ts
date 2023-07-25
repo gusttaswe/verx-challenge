aa4e0e399145fed2ea2f559f0a2ca29719b228a3
Gustavo Mata <dev.gustta.h@gmail.com>
2023-07-25 17:59:06 -0300

    test(Producer): define create-producer unit tests cases

---
+++ src/modules/Producer/create-producer/create-producer.usecase.spec.ts
@@ -0,0 +1,16 @@
+// Module
+import { CreateProducerUseCase } from './create-producer.usecase'
+
+describe('ListPosts', () => {
+  let createProducerUseCase: CreateProducerUseCase
+
+  it('Should be able to create a Producer Successfully', async () => {})
+
+  it('Should NOT be able to create a Producer with a incorrect Document', async () => {})
+
+  it('Should allow creating a farm when the total area is greater than or equal to the sum of cultivable and vegetation area ', async () => {})
+
+  it('Should throw an error when the sum of the cultivable and vegetation area is greater than the farm total area', async () => {})
+
+  it('Should allow creating a producer with multiple planted cultures', async () => {})
+})
