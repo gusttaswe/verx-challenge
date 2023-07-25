ddd28f859f626b90548e7f4b85d19d6ed89d34b0
Gustavo Mata <dev.gustta.h@gmail.com>
2023-07-25 18:35:46 -0300

    feat(middlewares): add auth guard with basic auth configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,12 @@
 import { Global, Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { APP_GUARD } from '@nestjs/core'
+
+// Modules
+import { ProducerModule } from 'modules/Producer/producer.module'
+
+// Middlewares
+import { AuthGuard } from 'shared/middlewares/auth.guard'
 
 // Envs
 import envs from 'shared/config/envs'
@@ -7,10 +14,17 @@ import envs from 'shared/config/envs'
 @Global()
 @Module({
   imports: [
+    ProducerModule,
     ConfigModule.forRoot({
       load: [envs],
       isGlobal: true
     })
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard
+    }
   ]
 })
 export class AppModule {}
---
+++ src/shared/middlewares/auth.guard.ts
@@ -0,0 +1,34 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+  ForbiddenException
+} from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { Observable } from 'rxjs'
+
+// Shared
+import { API, EnvVariables } from 'shared/config/envs'
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private readonly configService: ConfigService<EnvVariables>) {}
+
+  canActivate(
+    context: ExecutionContext
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest()
+    const { authorization } = request.headers
+
+    if (!authorization)
+      throw new UnauthorizedException('Unauthorized. Missing Tokens!')
+
+    const [prefix, token] = authorization.split(' ')
+    const apiEnv = this.configService.get<API>('API')
+    if (prefix !== 'Basic' || token !== apiEnv.auth)
+      throw new ForbiddenException("You're not allowed to access this resource")
+
+    return true
+  }
+}
