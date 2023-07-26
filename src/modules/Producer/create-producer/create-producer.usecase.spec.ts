fb42736cd0b593de7fdcb5a54c8c64f05d1cb813
Gustavo Mata <dev.gustta.h@gmail.com>
2023-07-26 12:40:10 -0300

    test(create-producer): add unit test to test producer creation

--- src/modules/Producer/create-producer/create-producer.usecase.spec.ts
+++ src/modules/Producer/create-producer/create-producer.usecase.spec.ts
@@ -1,16 +1,125 @@
-// Module
+import { Test, TestingModule } from '@nestjs/testing'
+import faker from 'faker-br'
+
+// UseCase
 import { CreateProducerUseCase } from './create-producer.usecase'
 
-describe('ListPosts', () => {
+// Repository Contracts
+import { IProducerRepository } from 'repositories/producer/producer.contract'
+import { IAddressRepository } from 'repositories/address/address.contract'
+import { ICultureRepository } from 'repositories/culture/culture.contract'
+import { IFarmRepository } from 'repositories/farm/farm.contract'
+
+// Fakes Implementations
+import { FakeProducerRepository } from 'repositories/producer/implementations/faker/producer.faker'
+import { FakeAddressRepository } from 'repositories/address/implementations/faker/address.faker'
+import { FakeCultureRepository } from 'repositories/culture/implementations/faker/culture.faker'
+import { FakeFarmRepository } from 'repositories/farm/implementations/faker/farm.faker'
+
+// Domains
+import { Producer } from 'domains/producer.domain'
+import { Address } from 'domains/address.domain'
+import { Culture, cultureTypes } from 'domains/culture.domain'
+import { Farm } from 'domains/farm.domain'
+
+// DTOS
+import {
+  CreateProducerInput,
+  CreateProducerOutput
+} from './create-producer.dto'
+
+const PRODUCER_MOCK: Omit<Producer, 'id'> = {
+  document: faker.br.cnpj(),
+  name: faker.name.firstName()
+}
+
+const ADDRESS_MOCK: Omit<Address, 'id'> = {
+  city: faker.address.city(),
+  state: faker.address.state()
+}
+
+const CULTURE_MOCK: Omit<Culture, 'id'>[] = [
+  {
+    name: cultureTypes.MILHO
+  },
+  {
+    name: cultureTypes.CAFE
+  }
+]
+
+describe('CreateProducer', () => {
   let createProducerUseCase: CreateProducerUseCase
+  let fakeProducerRepository: IProducerRepository
 
-  it('Should be able to create a Producer Successfully', async () => {})
+  beforeEach(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      providers: [
+        CreateProducerUseCase,
+        {
+          provide: IProducerRepository,
+          useClass: FakeProducerRepository
+        },
+        {
+          provide: IAddressRepository,
+          useClass: FakeAddressRepository
+        },
+        {
+          provide: ICultureRepository,
+          useClass: FakeCultureRepository
+        },
+        {
+          provide: IFarmRepository,
+          useClass: FakeFarmRepository
+        }
+      ]
+    }).compile()
 
-  it('Should NOT be able to create a Producer with a incorrect Document', async () => {})
+    createProducerUseCase = moduleRef.get<CreateProducerUseCase>(
+      CreateProducerUseCase
+    )
+    fakeProducerRepository =
+      moduleRef.get<IProducerRepository>(IProducerRepository)
+  })
 
-  it('Should allow creating a farm when the total area is greater than or equal to the sum of cultivable and vegetation area ', async () => {})
+  it('Should be able to create a Producer Successfully', async () => {
+    const createProducerMock: CreateProducerInput = {
+      producer: PRODUCER_MOCK,
+      farm: {
+        name: faker.company.companyName(),
+        totalHectares: 15,
+        cultivableHectares: 12,
+        vegetationHectares: 3,
+        address: ADDRESS_MOCK,
+        plantedCultures: CULTURE_MOCK
+      }
+    }
 
-  it('Should throw an error when the sum of the cultivable and vegetation area is greater than the farm total area', async () => {})
+    const result = await createProducerUseCase.execute(createProducerMock)
 
-  it('Should allow creating a producer with multiple planted cultures', async () => {})
+    const resultValue: CreateProducerOutput = result._unsafeUnwrap()
+
+    expect(result.isOk()).toBe(true)
+
+    expect(resultValue.producer).toBeInstanceOf(Producer)
+    expect(resultValue.producer).toHaveProperty('id')
+
+    expect(resultValue.farm).toBeInstanceOf(Farm)
+    expect(resultValue.farm).toHaveProperty('id')
+
+    expect(resultValue.farm.address).toBeInstanceOf(Address)
+    expect(resultValue.farm.address).toHaveProperty('id')
+
+    resultValue.farm.plantedCultures.forEach((culture) => {
+      expect(culture).toBeInstanceOf(Culture)
+      expect(culture).toHaveProperty('id')
+    })
+  })
+
+  // it('Should NOT be able to create a Producer with a incorrect Document', async () => {})
+
+  // it('Should allow creating a farm when the total area is greater than or equal to the sum of cultivable and vegetation area ', async () => {})
+
+  // it('Should throw an error when the sum of the cultivable and vegetation area is greater than the farm total area', async () => {})
+
+  // it('Should allow creating a producer with multiple planted cultures', async () => {})
 })
--- src/modules/Producer/create-producer/create-producer.usecase.ts
+++ src/modules/Producer/create-producer/create-producer.usecase.ts
@@ -1,11 +1,70 @@
 import { Injectable } from '@nestjs/common'
+import { Ok, Err, Result } from 'neverthrow'
 
-// Interfaces & Types
+// Repositories
+import { IProducerRepository } from 'repositories/producer/producer.contract'
+
+// UseCase
 import { UseCase } from 'shared/core/usecase'
 
-@Injectable()
-export class CreateProducerUseCase implements UseCase<any, any, any> {
-  constructor() {}
+// Interfaces & Types
+import {
+  CreateProducerInput,
+  CreateProducerOutput
+} from './create-producer.dto'
 
-  public async execute(): Promise<any> {}
+// Domains
+import { Producer } from 'domains/producer.domain'
+import { IAddressRepository } from 'repositories/address/address.contract'
+import { IFarmRepository } from 'repositories/farm/farm.contract'
+import { ICultureRepository } from 'repositories/culture/culture.contract'
+import { Address } from 'domains/address.domain'
+import { Culture } from 'domains/culture.domain'
+import { Farm } from 'domains/farm.domain'
+
+@Injectable()
+export class CreateProducerUseCase
+  implements UseCase<CreateProducerInput, CreateProducerOutput, any>
+{
+  constructor(
+    private readonly producerRepository: IProducerRepository,
+    private readonly addressRepository: IAddressRepository,
+    private readonly farmRepository: IFarmRepository,
+    private readonly cultureRepository: ICultureRepository
+  ) {}
+
+  public async execute(
+    input: CreateProducerInput
+  ): Promise<Result<CreateProducerOutput, any>> {
+    const producer = new Producer(input.producer)
+    await this.producerRepository.save(producer)
+
+    const address = new Address(input.farm.address)
+    await this.addressRepository.save(address)
+
+    const cultures = input.farm.plantedCultures.map(
+      (culture) => new Culture(culture)
+    )
+    const culturePromises = cultures.map((culture) =>
+      this.cultureRepository.save(culture)
+    )
+    await Promise.all(culturePromises)
+
+    const farm = new Farm({
+      name: input.farm.name,
+      totalHectares: input.farm.totalHectares,
+      cultivableHectares: input.farm.cultivableHectares,
+      vegetationHectares: input.farm.vegetationHectares,
+      address: address,
+      plantedCultures: cultures
+    })
+    await this.farmRepository.save(farm)
+
+    // TODO: return input + id
+    const output: CreateProducerOutput = {
+      producer,
+      farm
+    }
+    return new Ok(output)
+  }
 }
