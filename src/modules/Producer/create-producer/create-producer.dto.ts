82be066c98a663811445ba6457f7069400397ea1
Gustavo Mata <dev.gustta.h@gmail.com>
2023-07-27 02:38:48 -0300

    feat(modules): define CreateProducerOutput dto

--- src/modules/Producer/create-producer/create-producer.dto.ts
+++ src/modules/Producer/create-producer/create-producer.dto.ts
@@ -1,4 +1,4 @@
-import { OmitType } from '@nestjs/swagger'
+import { ApiProperty, OmitType } from '@nestjs/swagger'
 
 // Domains
 import { Producer } from 'domains/producer.domain'
@@ -16,11 +16,20 @@ class ProducerFarm extends OmitType(Farm, [
 }
 
 export class CreateProducerInput {
-  producer: Omit<Producer, 'id'>
+  producer: Omit<Producer, 'id' | 'document'> & { document: string }
   farm: ProducerFarm
 }
 
 export class CreateProducerOutput {
+  @ApiProperty({
+    description: 'Instance of a Producer',
+    type: Producer
+  })
   producer: Producer
+
+  @ApiProperty({
+    description: 'Instance of a Farm',
+    type: Farm
+  })
   farm: Farm
 }
