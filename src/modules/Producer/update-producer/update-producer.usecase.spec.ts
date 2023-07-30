d9b2457c1b553cc1f5d29e72d01f874b2398afbd
Gustavo Mata <dev.gustta.h@gmail.com>
2023-07-30 18:02:49 -0300

    test(update-producer): add unit unit test coverage

--- src/modules/Producer/update-producer/update-producer.usecase.spec.ts
+++ src/modules/Producer/update-producer/update-producer.usecase.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing'
-import { Err, Ok } from 'neverthrow'
+import { Err } from 'neverthrow'
 import { randomUUID } from 'crypto'
 import faker from 'faker-br'
 
@@ -21,12 +21,9 @@ import { InMemoryFarmRepository } from 'repositories/farm/implementations/in-mem
 
 // Domains
 import { Producer } from 'domains/producer.entity'
-import { Address } from 'domains/address.entity'
-import { Culture, cultureTypes } from 'domains/culture.entity'
-import { Farm } from 'domains/farm.entity'
 
 // DTOS
-import { UpdateProducerInput, UpdateProducerOutput } from './update-producer.dto'
+import { UpdateProducerInput } from './update-producer.dto'
 
 const PRODUCER_MOCKED: Producer = {
   id: randomUUID(),
@@ -34,27 +31,9 @@ const PRODUCER_MOCKED: Producer = {
   name: faker.name.firstName()
 }
 
-const FARM_MOCKED: Farm = {
-  id: randomUUID(),
-  name: faker.company.companyName(),
-  totalArea: 15,
-  cultivableArea: 12,
-  vegetationArea: 3,
-  address: {
-    id: randomUUID(),
-    city: faker.address.city(),
-    state: faker.address.state()
-  },
-  cultures: [
-    { id: randomUUID(), name: cultureTypes.MILHO },
-    { id: randomUUID(), name: cultureTypes.CAFE }
-  ]
-}
-
 describe('UpdateProducer', () => {
   let createProducerUseCase: UpdateProducerUseCase
   let inMemoryProducerRepository: IProducerRepository
-  let inMemoryFarmRepository: IFarmRepository
 
   beforeEach(async () => {
     const moduleRef: TestingModule = await Test.createTestingModule({
@@ -81,106 +60,58 @@ describe('UpdateProducer', () => {
 
     createProducerUseCase = moduleRef.get<UpdateProducerUseCase>(UpdateProducerUseCase)
     inMemoryProducerRepository = moduleRef.get<IProducerRepository>(IProducerRepository)
-    inMemoryFarmRepository = moduleRef.get<IFarmRepository>(IFarmRepository)
 
     // Mock in-memory-data
-    inMemoryFarmRepository['farms'].push(FARM_MOCKED)
-    inMemoryProducerRepository['producers'].push(PRODUCER_MOCKED)
+    inMemoryProducerRepository['producers'].push({ ...PRODUCER_MOCKED })
   })
 
   it('Should be able to update a Producer Successfully', async () => {
     const payload: UpdateProducerInput = {
-      producer: {
-        id: PRODUCER_MOCKED.id,
-        name: faker.name.firstName() // Changed
-      },
-      farm: {
-        ...FARM_MOCKED,
-        address: {
-          ...FARM_MOCKED.address,
-          city: 'Mogi das Cruzes' // Changed
-        },
-        name: 'Gustavo Mata', // Changed
-        totalArea: 100 // Changed
-      }
+      id: PRODUCER_MOCKED.id,
+      name: faker.name.firstName(), // Changed
+      document: faker.br.cpf() // Changed
     }
 
     const result = await createProducerUseCase.execute(payload)
-
     const resultValue = result._unsafeUnwrap()
-    expect(resultValue.id).toEqual(payload.producer.id)
-    expect(resultValue.name).toEqual(payload.producer.name)
-    expect(resultValue.document).toEqual(PRODUCER_MOCKED.document)
-    expect(resultValue.farms[0].name).toEqual(payload.farm.name)
-    expect(resultValue.farms[0].totalArea).toEqual(payload.farm.totalArea)
-    expect(resultValue.farms[0].address.city).toEqual(payload.farm.address.city)
+
+    expect(result.isOk()).toBe(true)
+    expect(resultValue.id).toEqual(payload.id)
+    expect(resultValue.name).toEqual(payload.name)
+    expect(resultValue.document).toEqual(payload.document)
   })
 
-  // it('Should NOT be able to create a Producer with a incorrect Document', async () => {
-  //   const createProducerMockInvalidDocument: UpdateProducerInput = {
-  //     ...updateProducerInputMock,
-  //     document: 'INVALID_DOCUMENT'
-  //   }
+  it('Should return an error if the document provided is invalid', async () => {
+    const payload: UpdateProducerInput = {
+      id: PRODUCER_MOCKED.id,
+      document: 'INVALID_DOCUMENT'
+    }
 
-  //   const result = await createProducerUseCase.execute(createProducerMockInvalidDocument)
-  //   expect(result.isErr()).toBe(true)
-  //   expect(result).toEqual(new Err(UpdateProducerError.InvalidDocument()))
-  // })
+    const result = await createProducerUseCase.execute(payload)
+    expect(result).toEqual(new Err(UpdateProducerError.InvalidDocument()))
+  })
 
-  // it('Should allow creating a farm when the total area is greater than or equal to the sum of cultivable and vegetation area', async () => {
-  //   const createProducerMock: UpdateProducerInput = {
-  //     ...updateProducerInputMock,
-  //     farm: {
-  //       ...updateProducerInputMock.farm,
-  //       totalArea: 20,
-  //       cultivableArea: 12,
-  //       vegetationArea: 3
-  //     }
-  //   }
+  it('Should return an error if the producer is not found', async () => {
+    const payload: UpdateProducerInput = {
+      id: randomUUID(),
+      document: faker.br.cnpj()
+    }
 
-  //   const result = await createProducerUseCase.execute(createProducerMock)
-  //   expect(result.isOk()).toBe(true)
-  // })
+    const result = await createProducerUseCase.execute(payload)
+    expect(result).toEqual(new Err(UpdateProducerError.ProducerNotFound()))
+  })
 
-  // it('Should throw an error when the sum of the cultivable and vegetation area is greater than the farm total area', async () => {
-  //   const createProducerMock: UpdateProducerInput = {
-  //     ...updateProducerInputMock,
-  //     farm: {
-  //       ...updateProducerInputMock.farm,
-  //       totalArea: 10,
-  //       cultivableArea: 12,
-  //       vegetationArea: 3
-  //     }
-  //   }
+  it('Should return an error if the update fails', async () => {
+    jest
+      .spyOn(inMemoryProducerRepository, 'update')
+      .mockImplementation(async () => new Err(Error('Unable to update producer')))
 
-  //   const result = await createProducerUseCase.execute(createProducerMock)
-  //   expect(result.isErr()).toBe(true)
-  //   expect(result).toEqual(new Err(UpdateProducerError.InsufficientFarmArea()))
-  // })
+    const payload: UpdateProducerInput = {
+      id: PRODUCER_MOCKED.id,
+      document: faker.br.cnpj()
+    }
 
-  // it('Should allow creating a producer with multiple planted cultures', async () => {
-  //   const result = await createProducerUseCase.execute(updateProducerInputMock)
-  //   const resultValue = result._unsafeUnwrap()
-  //   expect(result.isOk()).toBe(true)
-  //   expect(resultValue.farms[0]).toHaveProperty('cultures')
-  //   expect(resultValue.farms[0].cultures.length).toBeGreaterThan(1)
-  // })
-
-  // it('Should throw an error creating a producer with a invalid culture', async () => {
-  //   const createProducerMockInvalidCulture: UpdateProducerInput = updateProducerInputMock
-  //   createProducerMockInvalidCulture.farm.cultures = [{ name: 'INVALID_CULTURE' }]
-
-  //   const result = await createProducerUseCase.execute(createProducerMockInvalidCulture)
-  //   expect(result.isOk()).toBe(false)
-  //   expect(result).toEqual(new Err(UpdateProducerError.CultureNotFound()))
-  // })
-
-  // it('Should not be able to create two Producers with the same Document ', async () => {
-  //   const result = await createProducerUseCase.execute(updateProducerInputMock)
-  //   expect(result.isOk()).toBe(true)
-
-  //   expect(await createProducerUseCase.execute(updateProducerInputMock)).toEqual(
-  //     new Err(UpdateProducerError.ProducerAlreadyExists())
-  //   )
-  // })
+    const result = await createProducerUseCase.execute(payload)
+    expect(result).toEqual(new Err(UpdateProducerError.UnableToUpdateProducer()))
+  })
 })
--- src/modules/Producer/update-producer/update-producer.usecase.ts
+++ src/modules/Producer/update-producer/update-producer.usecase.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@nestjs/common'
 import { Ok, Err, Result } from 'neverthrow'
-import { UUID } from 'crypto'
 
 // Repositories
 import { IProducerRepository } from 'repositories/producer/producer.contract'
-import { ICultureRepository } from 'repositories/culture/culture.contract'
-import { IFarmRepository } from 'repositories/farm/farm.contract'
 
 // Module
 import { UseCase } from 'shared/core/usecase'
@@ -13,10 +10,7 @@ import { UpdateProducerError } from './update-producer.error'
 
 // Domain Entities
 import { Producer } from 'domains/producer.entity'
-import { Farm } from 'domains/farm.entity'
-import { Address } from 'domains/address.entity'
 import { Document } from 'domains/document.domain'
-import { Culture } from 'domains/culture.entity'
 
 // DTOs
 import { UpdateProducerInput, UpdateProducerOutput } from './update-producer.dto'
@@ -27,31 +21,23 @@ type UpdateProducerResponse = Result<UpdateProducerOutput, UpdateProducerError>
 export class UpdateProducerUseCase
   implements UseCase<UpdateProducerInput, UpdateProducerOutput, UpdateProducerError>
 {
-  constructor(
-    private readonly producerRepository: IProducerRepository,
-    private readonly farmRepository: IFarmRepository
-  ) {}
+  constructor(private readonly producerRepository: IProducerRepository) {}
 
   public async execute(input: UpdateProducerInput): Promise<UpdateProducerResponse> {
-    const [producerResult, farmResult] = await Promise.all([
-      this.producerRepository.getById(input.producer.id),
-      this.farmRepository.getById(input.farm.id)
-    ])
+    if (input.document) {
+      const documentOrError = Document.create(input.document)
+      if (documentOrError.isErr()) return new Err(UpdateProducerError.InvalidDocument())
+    }
+
+    const producerResult = await this.producerRepository.getById(input.id)
 
     if (producerResult.isErr()) return new Err(UpdateProducerError.ProducerNotFound())
     const producer: Producer = producerResult.value
 
-    if (farmResult.isErr()) return new Err(UpdateProducerError.FarmNotFound())
-    const farm: Farm = farmResult.value
+    const updatedProducer = Object.assign(producer, input)
 
-    const updatedProducer = Object.assign(producer, input.producer)
-    const updatedFarm = Object.assign(farm, input.farm)
-
-    const produceOrError = await this.producerRepository.update({
-      ...updatedProducer,
-      farms: [updatedFarm]
-    })
-    if (produceOrError.isErr()) return new Err(UpdateProducerError.UnableToCreateProducer())
+    const produceOrError = await this.producerRepository.update(updatedProducer)
+    if (produceOrError.isErr()) return new Err(UpdateProducerError.UnableToUpdateProducer())
 
     return new Ok(produceOrError.value)
   }
