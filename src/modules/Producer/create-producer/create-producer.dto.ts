292441a1a075e388df14063c89f8c7ec1c114aa8
Gustavo Mata <dev.gustta.h@gmail.com>
2023-07-30 01:34:56 -0300

    fix(create-producer-dto): fix swagger api properties

--- src/modules/Producer/create-producer/create-producer.dto.ts
+++ src/modules/Producer/create-producer/create-producer.dto.ts
@@ -1,17 +1,28 @@
-import { OmitType } from '@nestjs/swagger'
+import { ApiProperty, OmitType } from '@nestjs/swagger'
 
 // Domains
 import { Producer } from 'domains/producer.entity'
 import { Farm } from 'domains/farm.entity'
 import { Address } from 'domains/address.entity'
-import { Culture } from 'domains/culture.entity'
+import { Culture, cultureTypes } from 'domains/culture.entity'
 
 class ProducerFarm extends OmitType(Farm, ['id', 'address', 'cultures', 'producer'] as const) {
+  @ApiProperty({
+    type: OmitType(Address, ['id'] as const)
+  })
   address: Omit<Address, 'id'>
+
+  @ApiProperty({
+    example: [{ name: cultureTypes.ALGODAO }],
+    type: Culture
+  })
   cultures: Omit<Culture, 'id'>[]
 }
 
 export class CreateProducerInput extends OmitType(Producer, ['id', 'farms'] as const) {
+  @ApiProperty({
+    type: ProducerFarm
+  })
   farm: ProducerFarm
 }
 
