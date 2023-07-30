71fb7dc42ccd4ff7e62776c108cd295644261f74
Gustavo Mata <dev.gustta.h@gmail.com>
2023-07-30 18:27:20 -0300

    feat(repositories): add update and getById functions on producer.postgres repository

--- src/repositories/producer/implementations/postgres/producer.postgres.ts
+++ src/repositories/producer/implementations/postgres/producer.postgres.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
+import { UUID } from 'crypto'
 
 // Domains
 import { Producer } from 'domains/producer.entity'
 import { Document } from 'domains/document.domain'
 
 // configs
-import { Ok, Result } from 'shared/config/neverthrow.config'
+import { Err, Ok, Result } from 'shared/config/neverthrow.config'
 
 // Contract
 import { IProducerRepository } from 'repositories/producer/producer.contract'
@@ -20,8 +21,21 @@ export class PostgresProducerRepository implements IProducerRepository {
   ) {}
 
   async save(producer: Producer): Promise<Result<Producer, Error>> {
-    const producerCreated = await this.producerRepository.save(producer)
-    return new Ok(producerCreated)
+    try {
+      const producerCreated = await this.producerRepository.save(producer)
+      return new Ok(producerCreated)
+    } catch (err) {
+      return new Err(Error('Unable to create producer!'))
+    }
+  }
+
+  async update(producer: Producer): Promise<Result<Producer, Error>> {
+    try {
+      const producerUpdated = await this.producerRepository.save(producer)
+      return new Ok(producerUpdated)
+    } catch (err) {
+      return new Err(Error('Unable to update producer'))
+    }
   }
 
   async exists(document: Document): Promise<boolean> {
@@ -30,5 +44,10 @@ export class PostgresProducerRepository implements IProducerRepository {
         document: document.value
       }
     })
+  }
+
+  async getById(id: UUID): Promise<Result<Producer, Error>> {
+    const producer = await this.producerRepository.findOneBy({ id: id })
+    return producer ? new Ok(producer) : new Err(Error('Producer Not Found!'))
   }
 }
